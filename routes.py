cc46408c193b5dea6233258432a8eaee5b674b08
gab-2 <107018299+gab-2@users.noreply.github.com>
2024-06-08 19:09:55 -0300

    Alterações Gerais

--- routes.py
+++ routes.py
@@ -1,93 +1,64 @@
-from flask import Flask, request, jsonify
-from flask_restplus import Api, Resource, fields
+from flask import request, jsonify
 from models import Item, Comprador, Lance
 from database import db_session
 from datetime import datetime
+from flask_swagger import swagger  
+from flask_swagger_ui import get_swaggerui_blueprint  
 
-app = Flask(__name__)
-api = Api(app, version='1.0', title='Auction API', description='API de um Leilão')
+def setup_routes(app):
 
-item_model = api.model('Item', {
-    'id': fields.Integer(readOnly=True, description='The unique identifier of an item'),
-    'descricao': fields.String(required=True, description='Item description'),
-    'lance_inicial': fields.Float(required=True, description='Initial bid value'),
-})
+    @app.route('/spec')
+    def spec():
+        return jsonify(swagger(app))
 
-comprador_model = api.model('Comprador', {
-    'id': fields.Integer(readOnly=True, description='The unique identifier of a buyer'),
-    'nome': fields.String(required=True, description='Buyer name'),
-})
+    SWAGGER_URL = '/swagger'
+    API_URL = '/spec'
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': "API de Leilão"
+        }
+    )
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
-lance_model = api.model('Lance', {
-    'id': fields.Integer(readOnly=True, description='The unique identifier of a bid'),
-    'valor': fields.Float(required=True, description='Bid value'),
-    'item_id': fields.Integer(required=True, description='The ID of the item being bid on'),
-    'comprador_id': fields.Integer(required=True, description='The ID of the buyer making the bid'),
-})
-
-@api.route('/itens')
-class ItemList(Resource):
-    @api.doc('list_items')
-    @api.marshal_list_with(item_model)
-    def get(self):
-        """List all items"""
-        itens = Item.query.all()
-        response = [{'id': item.id, 'descricao': item.descricao, 'maior_lance': (item.lances[-1].valor if item.lances else item.lance_inicial), 'tempo_restante': (item.tempo_leilao - datetime.utcnow()).total_seconds()} for item in itens]
-        return response
-
-    @api.doc('create_item')
-    @api.expect(item_model)
-    @api.marshal_with(item_model, code=201)
-    def post(self):
-        """Create a new item"""
+    @app.route('/itens', methods=['POST'])
+    def cadastrar_item():
         data = request.get_json()
         item = Item(descricao=data['descricao'], lance_inicial=data['lance_inicial'])
         db_session.add(item)
         db_session.commit()
-        return item, 201
+        return jsonify({'id': item.id}), 201
 
-@api.route('/itens/<int:id>')
-@api.response(404, 'Item not found')
-@api.param('id', 'The item identifier')
-class Item(Resource):
-    @api.doc('get_item')
-    @api.marshal_with(item_model)
-    def get(self, id):
-        """Fetch an item given its identifier"""
-        item = Item.query.get(id)
-        if item:
-            response = {'id': item.id, 'descricao': item.descricao, 'maior_lance': (item.lances[-1].valor if item.lances else item.lance_inicial), 'tempo_restante': (item.tempo_leilao - datetime.utcnow()).total_seconds()}
-            return response
-        api.abort(404)
-
-@api.route('/compradores')
-class CompradorList(Resource):
-    @api.doc('create_comprador')
-    @api.expect(comprador_model)
-    @api.marshal_with(comprador_model, code=201)
-    def post(self):
-        """Create a new buyer"""
+    @app.route('/compradores', methods=['POST'])
+    def cadastrar_comprador():
         data = request.get_json()
         comprador = Comprador(nome=data['nome'])
         db_session.add(comprador)
         db_session.commit()
-        return comprador, 201
+        return jsonify({'id': comprador.id}), 201
 
-@api.route('/lances')
-class LanceList(Resource):
-    @api.doc('create_lance')
-    @api.expect(lance_model)
-    @api.marshal_with(lance_model, code=201)
-    def post(self):
-        """Create a new bid"""
+    @app.route('/lances', methods=['POST'])
+    def efetuar_lance():
         data = request.get_json()
         lance = Lance(valor=data['valor'], item_id=data['item_id'], comprador_id=data['comprador_id'])
         item = Item.query.get(data['item_id'])
         if item and lance.valor > (item.lances[-1].valor if item.lances else item.lance_inicial):
             db_session.add(lance)
             db_session.commit()
-            return lance, 201
-        api.abort(400, 'Invalid bid')
+            return jsonify({'id': lance.id}), 201
+        return jsonify({'erro': 'Lance inválido'}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    @app.route('/itens', methods=['GET'])
+    def listar_itens():
+        itens = Item.query.all()
+        response = [{'id': item.id, 'descricao': item.descricao, 'maior_lance': (item.lances[-1].valor if item.lances else item.lance_inicial), 'tempo_restante': (item.tempo_leilao - datetime.utcnow()).total_seconds()} for item in itens]
+        return jsonify(response)
+
+    @app.route('/itens/<int:id>', methods=['GET'])
+    def descricao_item(id):
+        item = Item.query.get(id)
+        if item:
+            response = {'id': item.id, 'descricao': item.descricao, 'maior_lance': (item.lances[-1].valor if item.lances else item.lance_inicial), 'tempo_restante': (item.tempo_leilao - datetime.utcnow()).total_seconds()}
+            return jsonify(response)
+        return jsonify({'erro': 'Item não encontrado'}), 404
