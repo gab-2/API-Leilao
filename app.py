a2936bee0031ed2de9ae9f860bd25af5a144b95c
gab-2 <107018299+gab-2@users.noreply.github.com>
2024-06-08 18:18:20 -0300

    Configure app to use PORT environment variable and fix dependencies

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from routes import setup_routes
 from database import init_db
+import os
 
 app = Flask(__name__)
 setup_routes(app)
 init_db(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
