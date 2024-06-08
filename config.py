a3b5089b5ca4c5435b24db78b68811069755668d
gab-2 <107018299+gab-2@users.noreply.github.com>
2024-06-08 18:02:38 -0300

    Fix database URI

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///auction.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///auction.db').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
